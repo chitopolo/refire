01158c61003e40c42fee05900775c87109711aa1
Rene Polo <chitopolo@MacBook-Pro-de-Rene.local>
2017-07-26 18:52:37 -0400

    level 1 added

---
+++ src/MiTitulo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MiTitulo = props => {
+    console.log("props:", props)
+  const style = { color: props.color };
+  return (
+    <div>
+      <h1 style={style}>
+        {props.titulo}
+      </h1>
+    </div>
+  );
+};
+
+export default MiTitulo;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MiTitulo from './MiTitulo';
 
-class App extends Component {
-    render () {
-        return (
-            <div>
-                <h1>Primer Componente</h1>
-            </div>
-        )
-    }
-}
+const MiPrimerComponente = () => {
+  return (
+    <div>
+      <MiTitulo titulo="Los props son excelentes!" color="rebeccapurple" />
+      <MiTitulo titulo="Usa props en todas partes!" color="mediumaquamarine" />
+      <MiTitulo titulo="Los props son los mejores" color="peru" />
+    </div>
+  );
+};
 
-ReactDOM.render(<App/>, document.getElementById('main'))
-
-export default App
+ReactDOM.render(<MiPrimerComponente />, document.getElementById('main'));

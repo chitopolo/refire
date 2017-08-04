696c59dce142498250751556cb9b2c8bc8c77e54
Rene Polo <chitopolo@MacBook-Pro-de-Rene.local>
2017-08-04 13:27:06 -0400

    a space deleted

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import MiTitulo from './MiTitulo';
 
 var MiPrimerComponente = function () {  
   return (
-
     React.createElement('div', null,
       React.createElement(MiTitulo, {titulo: 'Los props son excelentes!', color:"rebeccapurple"}),
       React.createElement(MiTitulo, {titulo: 'Usa props en todas partes!', color:'mediumaquamarine'}),

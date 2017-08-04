18e6714555faaa903b89f506199b7f9955b2c2c3
Rene Polo <chitopolo@MacBook-Pro-de-Rene.local>
2017-08-04 13:41:20 -0400

    component lyfecicle added

--- src/MiTitulo.js
+++ src/MiTitulo.js
@@ -1,15 +1,62 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react'
+import {Button, Glyphicon} from 'react-bootstrap'
 
-const MiTitulo = props => {
-    console.log("props:", props)
-  const style = { color: props.color };
-  return (
-    <div>
+class MiTitulo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      number: 0
+    }
+    this.plusOne = this.plusOne.bind(this)
+  }
+
+  componentWillMount () {
+
+  }
+
+  componentDidMount () {
+
+  }
+
+  componentWillReceiveProps (nextProps) {
+
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    return true
+  }
+
+  componentWillUpdate (nextProps, nextState) {
+
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+
+  }
+
+  componentWillUnmount () {
+
+  }
+
+  plusOne(){
+    this.setState({
+      number: this.state.number+1
+    })
+  }
+
+
+  render () {
+    const style = { color: this.props.color };
+    return (
+      <div>
       <h1 style={style}>
-        {props.titulo}
+        {this.props.titulo}  <Button onClick={this.plusOne} bsStyle="primary"><Glyphicon glyph="thumbs-up"/></Button>{this.state.number}
       </h1>
     </div>
-  );
-};
+    )
+  }
+}
 
-export default MiTitulo;
+
+export default MiTitulo
+
